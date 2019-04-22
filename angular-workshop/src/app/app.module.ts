4b351a7b4bd508a8432c3c466a934d677e2a0a2b
Oscar Martin <okrmartin@gmail.com>
2019-04-22 17:37:15 +0100

    adds components

--- angular-workshop/src/app/app.module.ts
+++ angular-workshop/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
